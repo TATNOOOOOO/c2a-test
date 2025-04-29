3238a84689a569bf7ae5dceb8589a708a34e1d58
TATNOOOOOO <167925266+TATNOOOOOO@users.noreply.github.com>
2025-04-29 18:00:58 +0900

    Update code.js

--- JS/code.js
+++ JS/code.js
@@ -2,7 +2,7 @@ $(function(){
     var ua = navigator.userAgent;
     if(ua.indexOf("iPhone") > 0 || ua.indexOf("Android") > 0 && ua.indexOf("Mobile") > 0){
     console.log("None recommanded");
-    alert("携帯電話での閲覧は推奨されません。")
+    alert("携帯電話での閲覧は推奨されません")
     }else{
     console.log("Recommanded");
     }
